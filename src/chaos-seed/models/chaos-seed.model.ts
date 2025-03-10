34bec5f1dce01c35d5e1a0f9fe750664425549b5
parkerrosenbauer <parkerrosenbauer@gmail.com>
2025-03-09 22:52:43 -0500

    added for DB integration

---
+++ src/chaos-seed/models/chaos-seed.model.ts
@@ -0,0 +1,99 @@
+import {
+  AllowNull,
+  AutoIncrement,
+  Column,
+  Model,
+  PrimaryKey,
+  Table,
+} from 'sequelize-typescript';
+import { Experienced } from '../interfaces';
+import { Identifiable } from 'src/common/interfaces';
+
+@Table({
+  timestamps: true,
+})
+export class ChaosSeed
+  extends Model<ChaosSeed>
+  implements Experienced, Identifiable
+{
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  declare id: number;
+
+  @AllowNull(false)
+  @Column
+  declare startingLocation: string;
+
+  @Column({ defaultValue: 'Unknown' })
+  declare name: string;
+
+  @Column({ defaultValue: 1 })
+  declare level: number;
+
+  @Column({ defaultValue: 0 })
+  declare xp: number;
+
+  @Column({ defaultValue: 0 })
+  declare alignment: number;
+
+  @Column({ defaultValue: 'Level 1 Who are you again?' })
+  declare reputation: string;
+
+  @Column({ defaultValue: 100 })
+  declare health: number;
+
+  @Column({ defaultValue: 100 })
+  declare mana: number;
+
+  @Column({ defaultValue: 100 })
+  declare stamina: number;
+
+  @Column({ defaultValue: 10 })
+  declare strength: number;
+
+  @Column({ defaultValue: 10 })
+  declare agility: number;
+
+  @Column({ defaultValue: 10 })
+  declare dexterity: number;
+
+  @Column({ defaultValue: 10 })
+  declare constitution: number;
+
+  @Column({ defaultValue: 10 })
+  declare endurance: number;
+
+  @Column({ defaultValue: 10 })
+  declare intelligence: number;
+
+  @Column({ defaultValue: 10 })
+  declare wisdom: number;
+
+  @Column({ defaultValue: 10 })
+  declare charisma: number;
+
+  @Column({ defaultValue: 10 })
+  declare luck: number;
+
+  @Column({ defaultValue: 'None ' })
+  declare resistances: string;
+
+  @Column({ defaultValue: 'None' })
+  declare skills: string;
+
+  @Column({ defaultValue: 'None' })
+  declare marks: string;
+
+  @Column({ defaultValue: false })
+  declare isDeadOnArrival: boolean;
+
+  @Column
+  declare abilities?: string;
+
+  @Column
+  declare languages?: string;
+
+  @Column
+  declare race?: string;
+}
