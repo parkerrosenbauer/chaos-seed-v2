01a77fa177f04a5dcfed0d31e096f9d7239f3543
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-03-11 21:01:43 -0500

    added db layer to docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,25 +8,21 @@ import { logger } from './middleware';
 import { ChaosSeedsModule } from './chaos-seed/chaos-seeds.module';
 import { APP_PIPE } from '@nestjs/core';
 import { SequelizeModule } from '@nestjs/sequelize';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     ChaosSeedsModule,
     ConfigModule.forRoot(),
-    SequelizeModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        dialect: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
-        autoLoadModels: true,
-        synchronize: true,
-      }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: 5400,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      synchronize: true,
     }),
   ],
   providers: [
