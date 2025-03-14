aa38770d7fdcd9015525cddfee1da9ad1b4d2fb9
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-03-13 21:06:50 -0500

    added new association exports

--- src/chaos-seed/models/index.ts
+++ src/chaos-seed/models/index.ts
@@ -2,3 +2,5 @@ export { ChaosSeed } from './chaos-seed.model';
 export { Race } from './race.model';
 export { Language } from './language.model';
 export { Ability } from './ability.model';
+export { ChaosSeedAbility } from './chaos-seed-ability.model';
+export { ChaosSeedLanguage } from './chaos-seed-language.model';
